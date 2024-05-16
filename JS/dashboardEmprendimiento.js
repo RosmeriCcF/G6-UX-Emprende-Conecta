792d75433ea2850e5009b68ba29c7e0347910fe2
Dafna Nicole <111672427+thvx@users.noreply.github.com>
2024-05-16 14:31:38 -0500

    Linkeando dashboard

---
+++ JS/dashboardEmprendimiento.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const addToCartButton = document.querySelector(".add-to-cart");
+
+    addToCartButton.addEventListener("click", function() {
+        window.location.href = "pagina3.html";
+    });
+});
